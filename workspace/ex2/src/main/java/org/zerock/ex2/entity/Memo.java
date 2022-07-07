eb396323f7750c13d5e42907be131f88e104ee53
Pinixk <bjkim0110@gmail.com>
2022-07-07 12:50:25 +0900

    07/07

--- workspace/ex2/src/main/java/org/zerock/ex2/entity/Memo.java
+++ workspace/ex2/src/main/java/org/zerock/ex2/entity/Memo.java
@@ -1,8 +1,22 @@
 package org.zerock.ex2.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity
+import lombok.ToString;
+
+@Entity // spl table
+@Table(name = "tbl_memo") // table 이름 지정
+@ToString // hash code를 string타입으로 변환
 public class Memo {
+  @Id // primary key
+  @GeneratedValue(strategy = GenerationType.IDENTITY) // autoincrement
   private Long mno;
+
+  @Column(length = 200, nullable = false)
+  private String memoText;
 }
