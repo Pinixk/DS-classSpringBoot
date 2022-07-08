2cd8f2d1c8bdb7e9db208c4255924c6909d15a92
Pinixk <bjkim0110@gmail.com>
2022-07-08 15:33:03 +0900

    07/08

--- workspace/ex3/src/main/java/org/zerock/ex3/controller/SampleController.java
+++ workspace/ex3/src/main/java/org/zerock/ex3/controller/SampleController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.ex3.dto.SampleDTO;
 
 import lombok.extern.log4j.Log4j2;
@@ -20,19 +21,19 @@ import lombok.extern.log4j.Log4j2;
 public class SampleController {
   @GetMapping("/ex1")
   public void ex1(Model model) {
-    // void일 때요청된 url과 resource파일이 일치함
+    // void일 때 요청된 url과 resource파일이 일치함
     // return 필요하지 않음
     log.info("ex1/.................");
 
     model.addAttribute("title", "Hello ex1");
   }
 
-  @GetMapping("/ex2")
-  public void ex2(Model model) {
+  @GetMapping({"/ex2","/exLink"})
+  public void ex2(Model model) { // view의 내용을 보내주기 위함
     log.info("ex2/.................");
     model.addAttribute("title", "Hello ex2");
     // ("key","value")
-    
+
     List<SampleDTO> list = IntStream.rangeClosed(1, 20)
         .asLongStream()
         .mapToObj(new LongFunction<SampleDTO>() {
@@ -48,9 +49,28 @@ public class SampleController {
             return dto;
           }
         }).collect(Collectors.toList());
-
     model.addAttribute("list", list);
     // return "/sample/ex1"; // 경로 표시
+
+  }
+
+  @GetMapping({ "/exInline" })
+  public String exInline(RedirectAttributes ra) {
+    log.info("exInline.............");
+    SampleDTO dto = SampleDTO.builder()
+        .sno(100L).first("First...100")
+        .last("Last...100")
+        .regTime(LocalDateTime.now())
+        .build();
+    ra.addFlashAttribute("result", "success");
+    ra.addFlashAttribute("dto", dto);
+    // 일회성
+    return "redirect:/sample/ex3";
+  }
+
+  @GetMapping("/ex3")
+  public void ex3() {
+    log.info("ex3...........");
   }
 
 }
