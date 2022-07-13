69ca7796d980e0c8576472befd0281fb22b8cebd
Pinixk <bjkim0110@gmail.com>
2022-07-13 12:44:21 +0900

    07/13

--- workspace/guestbook/src/main/java/org/zerock/guestbook/controller/GuestbookController.java
+++ workspace/guestbook/src/main/java/org/zerock/guestbook/controller/GuestbookController.java
@@ -35,24 +35,21 @@ public class GuestbookController {
 
   @GetMapping("/register")
   public void register() {
-    log.info("register get..............");
-  }
-
+    log.info("register get..............");}
   @PostMapping("/register")
   public String registPost(GuestbookDTO dto, RedirectAttributes ra) { // 커맨드 객체
     // RedirectAttributes 모델처럼 다음 객체에게 정보를 전달 할 수 있음
-
     log.info("register post.................");
     Long gno = gbService.register(dto);
     ra.addFlashAttribute("msg", gno); // 중복으로 값이 오는 것을 막기 위해
     return "redirect:/guestbook/list"; // /list에서 받은 값을 간단히 다시 쓰기 위해 resource로 보내지 않고 주소로 보냄
   }
 
-  @GetMapping("/read")
+  @GetMapping({"/read", "/modify"})
   public void read(Long gno, Model model,
       @ModelAttribute("requestDTO") PageRequestDTO requestDTO) { // 커맨드 객체
       // @ModelAttribute 변수명을 임의로 지정
-      
+
     log.info("read................ gno : " + gno);
     GuestbookDTO dto = gbService.read(gno);
     model.addAttribute("dto", dto);
