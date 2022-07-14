00011aea0f27eb4053d1d6a16188c336889de889
Pinixk <bjkim0110@gmail.com>
2022-07-14 14:43:05 +0900

    07/14

---
+++ workspace/board/src/main/java/org/zerock/board/dto/PageRequestDTO.java
@@ -0,0 +1,30 @@
+package org.zerock.board.dto;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@AllArgsConstructor
+@Data
+public class PageRequestDTO { // 어떤 페이지를 요청할지
+
+  private int page;
+  private int size;
+  private String type;
+  private String keyword;
+  
+  public PageRequestDTO(){
+    page = 1;
+    size = 10;
+  }
+
+  public Pageable getPageable(Sort sort){
+    return PageRequest.of(page-1, size, sort);  // jpa의 페이지는 0부터 시작이기 때문
+  }
+  
+}
