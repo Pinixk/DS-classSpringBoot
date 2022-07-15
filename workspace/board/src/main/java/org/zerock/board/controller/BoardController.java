1a86bce8f60d2dffd33c14691e3f948c7de4cf43
Pinixk <bjkim0110@gmail.com>
2022-07-15 16:10:00 +0900

    07/15

--- workspace/board/src/main/java/org/zerock/board/controller/BoardController.java
+++ workspace/board/src/main/java/org/zerock/board/controller/BoardController.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.board.dto.BoardDTO;
 import org.zerock.board.dto.PageRequestDTO;
 import org.zerock.board.service.BoardService;
 
@@ -18,9 +21,61 @@ import lombok.extern.log4j.Log4j2;
 public class BoardController {
   private final BoardService service;
 
-  @GetMapping({"/list",""})
+  @GetMapping({ "/","" })
+  public String index() {
+    log.info("index.................");
+    return "redirect:/board/list";
+  }
+
+  @GetMapping("/list")
   public void list(Model model, @ModelAttribute("requestDTO") PageRequestDTO requestDTO) {
     log.info("list..."+requestDTO);
     model.addAttribute("result", service.getList(requestDTO));
   }
+
+  @GetMapping("/register")
+  public void register(){
+    log.info("register...");}
+  @PostMapping("/register")
+  public String registerPost(BoardDTO dto, RedirectAttributes ra){
+    log.info("register Post..."+dto);
+
+    Long bno = service.register(dto);
+    ra.addFlashAttribute("msg",bno+" 등록");
+
+    return "redirect:/board/list";
+  }
+
+  @GetMapping({"/read", "/modify"})
+  public void read(Long bno, Model model,
+      @ModelAttribute("requestDTO") PageRequestDTO requestDTO) { // 커맨드 객체
+      
+    log.info("read................ bno : " + bno);
+    BoardDTO dto = service.get(bno);
+    model.addAttribute("dto", dto);
+  }
+
+  // @PostMapping("/remove")
+  // public String remove(Long bno, RedirectAttributes ra, PageRequestDTO requestDTO){
+  //   log.info("remove.............." + bno);
+  //   service.remove(bno);
+  //   ra.addFlashAttribute("msg", bno+" 삭제");
+  //   ra.addAttribute("page", requestDTO.getPage());
+  //   ra.addAttribute("type", requestDTO.getType());
+  //   ra.addAttribute("keyword", requestDTO.getKeyword());
+  //   return "redirect:/guestbook/list";
+  // }
+
+  @PostMapping("/modify")
+  public String modifyPost(BoardDTO dto, RedirectAttributes ra, PageRequestDTO requestDTO){
+    log.info("modify................" + dto);
+    service.modify(dto);
+    ra.addFlashAttribute("msg", dto.getBno()+" 수정");
+    ra.addAttribute("bno", dto.getBno());
+    ra.addAttribute("page", requestDTO.getPage());
+    ra.addAttribute("type", requestDTO.getType());
+    ra.addAttribute("keyword", requestDTO.getKeyword());
+    return "redirect:/guestbook/read";
+  }
+
 }
