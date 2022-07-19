7ab38199a16f0171d90afe040d3f98ab81a859b1
Pinixk <bjkim0110@gmail.com>
2022-07-19 17:47:43 +0900

    07/19

--- workspace/board/src/main/java/org/zerock/board/controller/ReplyController.java
+++ workspace/board/src/main/java/org/zerock/board/controller/ReplyController.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.zerock.board.dto.ReplyDTO;
@@ -30,6 +34,23 @@ public class ReplyController {
     log.info("bno : " + bno);
     return new ResponseEntity<>(service.getList(bno), HttpStatus.OK);
 
+  }
+
+  @PostMapping("")
+  public ResponseEntity<Long> register(@RequestBody ReplyDTO dto){
+    return new ResponseEntity<>(service.register(dto), HttpStatus.OK);
+  }
+
+  @PutMapping("/{rno}")
+  public ResponseEntity<String> modify(@RequestBody ReplyDTO dto){
+    service.modify(dto);
+    return new ResponseEntity<>("success", HttpStatus.OK);
+  }
+
+  @DeleteMapping("/{rno}")
+  public ResponseEntity<String> remove(@PathVariable("rno") Long rno){
+    service.remove(rno);
+    return new ResponseEntity<>("success", HttpStatus.OK);
   }
 
 }
